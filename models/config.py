34f2148af490e7ac72191ce9f10523bf46b53f1f
SamZec <samzec4@gmail.com>
2024-10-04 10:46:07 +0000

    Resolve issues with database

--- models/config.py
+++ models/config.py
@@ -1,4 +1,4 @@
 #!/bin/python3
 """Mongo engine connection strings"""
 
-host = 'mongodb+srv://samzec4:meY5iGOhEioaYLLY@cluster0.ya7up.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+host = ${{ MongoDB.MONGO_URL }}
